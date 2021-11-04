2db6b4fff01b1338dd345c243a32ad665df2126d
Michael-F-Bryan <michael@hotg.ai>
2021-11-05 02:11:48 +0800

    Upgraded @hotg-ai/rune

--- lessons/lesson-4/src/App.tsx
+++ lessons/lesson-4/src/App.tsx
@@ -1,56 +1,45 @@
 import React, { FormEvent, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { builder, Capability } from '@hotg-ai/rune';
+import { builder, } from '@hotg-ai/rune';
 import { Result } from '@hotg-ai/rune/dist/Builder';
 import '@tensorflow/tfjs-tflite/dist/tflite_web_api_cc_simd.js';
+import { ReadInput } from '@hotg-ai/rune/dist/facade';
+import { Tensor } from '@tensorflow/tfjs-core';
 
-type RunFunc = () => Result;
+type RunFunc = (r: ReadInput) => Result;
 
-const inputs = {
-  paragraph: "",
-  question: "",
-};
-
-function App() {
-  const [run, setRun] = useState<RunFunc>();
-  const [rune, setRune] = useState<ArrayBuffer>();
+export default function App() {
+  const [runtime, setRuntime] = useState<RunFunc>();
   const [paragraph, setParagraph] = useState("");
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
 
-  // Fetch the Rune on startup
   useEffect(() => {
-    fetch("/lesson-4.rune")
-      .then(r => r.arrayBuffer())
-      .then(buffer => setRune(buffer))
-      .then();
-  });
+    console.log("Loading the Rune");
 
-  // TODO: Figure out a less hacky way to
-  inputs.paragraph = paragraph;
-  inputs.question = question;
+    // Fetch the Rune on startup
+    builder()
+      .build("/lesson-4.rune")
+      .then(r => {
+        console.log("Loaded the Rune");
+        setRuntime(r);
+      })
+      .catch(console.error);
+  }, []);
 
   const submit = (e: FormEvent<HTMLFormElement>) => {
+    console.log("Clicked the ASK button", { runtime, paragraph, question, answer });
     e.preventDefault();
-    if (!run) {
+    if (!runtime) {
       return;
     }
 
-    const result = run();
+    console.log("Running the Rune");
+    const result = runtime(input => generateInput(question, paragraph));
     const [textOutput] = result.outputs;
     setAnswer(textOutput.value);
   };
-
-  const pressed = () => builder()
-    .onDebug(console.log)
-    .withCapability("raw", () => new TextCapability())
-    .build("/lesson-4.rune")
-    .then(runtime => setRun(runtime));
-
-  const button = run
-    ? (<input type="submit" value="Ask" />)
-    : (<button onClick={pressed}>Fetch Rune</button>);
 
   return (
     <div className="App">
@@ -73,7 +62,7 @@ function App() {
                   onChange={e => setQuestion(e.target.value)} />
               </label>
             </div>
-            {button}
+            <input type="submit" value="ASK" />
           </form>
 
           <div>
@@ -86,33 +75,7 @@ function App() {
   );
 }
 
-export default App;
-
-class TextCapability implements Capability {
-  source: number = 0;
-  encoder = new TextEncoder();
-
-  generate(dest: Uint8Array): void {
-    let text;
-    switch (this.source) {
-      case 0:
-        text = inputs.paragraph;
-        break;
-      case 1:
-        text = inputs.question;
-        break;
-      default:
-        throw new Error();
-    }
-
-    this.encoder.encodeInto(text, dest);
-  }
-
-  setParameter(name: string, value: number): void {
-    switch (name) {
-      case "source":
-        this.source = value;
-    }
-  }
-
+function generateInput(question: string, paragraph: string): Tensor {
+  throw new Error('Function not implemented.');
 }
+
